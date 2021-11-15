4e77cf335ae688529d09e5a0325c04fd67bdf6dd
Irina <hallo.ween@mail.ru>
2021-11-15 22:14:39 +0300

    initial commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import AppHeader from '../appHeader/AppHeader';
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppHeader />
+    </div>
+  )
+}
+
+export default App;
