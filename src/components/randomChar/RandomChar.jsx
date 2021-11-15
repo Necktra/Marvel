7e17a8e1b906ea55a1ee3f86b52e9ac13e02cdf5
Irina <hallo.ween@mail.ru>
2021-11-15 22:23:51 +0300

    randomchar component update

---
+++ src/components/randomChar/RandomChar.jsx
@@ -0,0 +1,16 @@
+import './randomChar.scss';
+
+const RandomChar = () => {
+    return (
+        <div className="randomchar">
+            <div className="randomchar__block">
+
+            </div>
+            <div className="randomchar__static">
+                
+                </div>
+        </div>
+    )
+}
+
+export default RandomChar;
