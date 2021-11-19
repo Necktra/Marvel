dbfce19f348fa580df76d73f5992659f827dc46c
Ira <hallo.ween@mail.ru>
2021-11-19 11:56:16 +0300

    RandomChar component update

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -7,15 +7,20 @@ import Spiner from '../spiner/Spiner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         char: {},
         loading: true,
         error: false
+    }
+
+    componentDidMount() {
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
     }
 
     onError = () => {
@@ -26,6 +31,12 @@ class RandomChar extends Component {
 
     onCharLoaded = (char) => {
         this.setState({char, loading: false})
+    }
+
+    tryRandomChar = () => {
+        this.updateChar();
+        clearInterval(this.timerId);
+        this.timerId = setInterval(this.updateChar, 3000);
     }
 
     updateChar = () => {
@@ -49,7 +60,7 @@ class RandomChar extends Component {
                 <div className="randomchar__static">
                     <p className="randomchar__title">Random character for today!<br />Do you want to get to know him better?</p>
                     <p className="randomchar__title">Or choose another one</p>
-                    <button className="button button__main">
+                    <button onClick={this.tryRandomChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={decoration} alt="Decoration" className="randomchar__decoration" />
@@ -63,8 +74,13 @@ const View = ({char}) => {
 
     const {name, description, thumbnail, homepage, wiki} = char;
 
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
     return(<div className="randomchar__block">
-    <img src={thumbnail} alt="Random character" className="randomchar__img" />
+    <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
 
     <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
