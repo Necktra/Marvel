2c55b2140e3397538dc0f439638d850beca523a2
Ira <hallo.ween@mail.ru>
2021-11-25 17:03:30 +0300

    add routing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,43 +1,28 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppHeader from '../appHeader/AppHeader';
-import CharInfo from '../charInfo/CharInfo';
-import CharList from '../charList/CharList';
-import RandomChar from '../randomChar/RandomChar';
-import decoration from '../../resources/img/Vision.png';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import AppBanner from '../appBanner/AppBanner';
-import ComicsList from '../comicsList/ComicsList';
+import {MainPage, ComicsPage} from '../pages';
+
 
 const App = () => {
 
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id)
-  }
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <AppBanner/>
-        <ComicsList/>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Routes>
 
+            <Route exact path="/comics"
+              element={<ComicsPage />}
+            />
 
-        {/* <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img src={decoration} alt="Vision" className="bg-decoration" /> */}
-      </main>
-    </div>
+            <Route exact path="/"
+              element={<MainPage />} />
+
+          </Routes>
+        </main>
+      </div>
+    </Router >
   )
 
 }
